04f057072f9172ec1c1818fa6fe3c38c7f8957f5
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-02-17 20:11:54 +0530

    Dashboard Updated

--- frontend/src/Pages/Dashboard/Index.js
+++ frontend/src/Pages/Dashboard/Index.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { MdRefresh } from "react-icons/md";
 
 function Index() {
   return (
     <>
-      This is dashboard! 
+      <div className='flex flex-col items-center h-full w-full bg-red-400'>
+        <span className='w-5/6 text-left font-semibold text-5xl'>
+          Smart Waste Management
+        </span>
+
+        <nav className='mt-16 w-5/6 flex justify-around items-center'>
+          <span>
+            <MdRefresh size={"30px"}/>
+          </span>
+          <div className='w-3/5 border-white border-2 h-12'>
+            
+          </div>
+          <span>All</span>
+        </nav>
+      </div>
     </>
   )
 }
